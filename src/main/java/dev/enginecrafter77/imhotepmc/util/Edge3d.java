cb9bbbd181299ffcac1c1434e998ad1535b14653
Enginecrafter77 <hutiramichal@gmail.com>
2023-11-26 23:51:15 +0100

    fix: Edge3d typo error in vector copy

--- src/main/java/dev/enginecrafter77/imhotepmc/util/Edge3d.java
+++ src/main/java/dev/enginecrafter77/imhotepmc/util/Edge3d.java
@@ -28,8 +28,8 @@ public class Edge3d {
 
 	public void set(double x1, double y1, double z1, double x2, double y2, double z2)
 	{
-		this.p1.set(x1, y1, y1);
-		this.p2.set(x2, y2, y2);
+		this.p1.set(x1, y1, z1);
+		this.p2.set(x2, y2, z2);
 	}
 
 	public void getFirstPoint(Tuple3d dest)
