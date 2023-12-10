ac73e6db181bdb54d2e700417e2688956adaef60
Enginecrafter77 <hutiramichal@gmail.com>
2023-12-10 02:33:12 +0100

    misc: Display shape card mode in name

--- src/main/java/dev/enginecrafter77/imhotepmc/item/ItemShapeCard.java
+++ src/main/java/dev/enginecrafter77/imhotepmc/item/ItemShapeCard.java
@@ -2,7 +2,6 @@ package dev.enginecrafter77.imhotepmc.item;
 
 import dev.enginecrafter77.imhotepmc.ImhotepMod;
 import dev.enginecrafter77.imhotepmc.tile.TerraformMode;
-import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -10,10 +9,6 @@ import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentTranslation;
-import net.minecraft.world.World;
-
-import javax.annotation.Nullable;
-import java.util.List;
 
 public class ItemShapeCard extends Item {
 	public ItemShapeCard()
@@ -35,13 +30,11 @@ public class ItemShapeCard extends Item {
 	}
 
 	@Override
-	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
+	public String getItemStackDisplayName(ItemStack stack)
 	{
-		super.addInformation(stack, worldIn, tooltip, flagIn);
 		TerraformMode mode = this.getMode(stack);
-
-		ITextComponent label = new TextComponentTranslation("label.terraformer.mode").appendText(": ").appendSibling(mode.getTranslatedName());
-		tooltip.add(label.getFormattedText());
+		ITextComponent label = new TextComponentTranslation(this.getTranslationKey(stack) + ".name").appendText(": ").appendSibling(mode.getTranslatedName());
+		return label.getFormattedText();
 	}
 
 	@Override
