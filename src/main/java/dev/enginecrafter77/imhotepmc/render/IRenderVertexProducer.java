54626214cbe34ad8c5e48b3edee56805ae962592
Enginecrafter77 <hutiramichal@gmail.com>
2023-12-10 02:35:18 +0100

    refactor: Remove leftover class IRenderVertexProducer.java

--- src/main/java/dev/enginecrafter77/imhotepmc/render/IRenderVertexProducer.java
+++
@@ -1,7 +0,0 @@
-package dev.enginecrafter77.imhotepmc.render;
-
-import net.minecraft.client.renderer.BufferBuilder;
-
-public interface IRenderVertexProducer {
-	public void render(BufferBuilder builder);
-}
