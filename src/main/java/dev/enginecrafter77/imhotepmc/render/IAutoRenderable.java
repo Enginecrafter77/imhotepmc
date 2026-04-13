0e6f8048d2c0a466a50e239c42000cd1b866edd3
Enginecrafter77 <hutiramichal@gmail.com>
2026-04-14 00:50:23 +0200

    refactor: Remove unused render classes

--- src/main/java/dev/enginecrafter77/imhotepmc/render/IAutoRenderable.java
+++
@@ -1,7 +0,0 @@
-package dev.enginecrafter77.imhotepmc.render;
-
-/** @deprecated Use {@link TesselatorRenderable} */
-@Deprecated
-public interface IAutoRenderable {
-	public void doRender(float partialTicks);
-}
